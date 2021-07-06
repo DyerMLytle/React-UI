d30a0a13834a380022cc0eee68484f976fa28a1d
Dyer Lytle <dyer@LytleDrive.local>
2021-07-05 20:34:54 -0700

    Working throught the React book.

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import Products from './Products';
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
+    //const isValid = true;
+
     return(
       <div>
         <h1>My First React App!</h1>
         <Products />
+        <Button variant="outline-primary" >Default</Button>
       </div>
     )
   }
