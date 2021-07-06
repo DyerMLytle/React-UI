35c52932041f3215b2d44ae2ee64cfceecfc5da6
dyerlytle <dlytle@lowell.edu>
2021-07-06 09:34:14 -0700

    Still working through book.

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
-import Products from './Products';
-import { Button } from 'react-bootstrap';
+// import Products from './Products';
+// import { Button } from 'react-bootstrap';
+import Rating from './Rating';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
-    //const isValid = true;
-
     return(
       <div>
-        <h1>My First React App!</h1>
-        <Products />
-        <Button variant="outline-primary" >Default</Button>
+       <Rating rating="1"/>
+       <Rating rating="2"/>
+       <Rating rating="3"/>
+       <Rating rating="4"/>
+       <Rating rating="5"/>
       </div>
-    )
+    );
   }
 }
 
