2339a278a78100cbff76fc052b7967e633d115df
Dyer Lytle <dyer@LytleDrive.local>
2021-07-06 15:26:49 -0700

    Working through book.

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from "react";
-// import Products from './Products';
+import Products from './Products';
 // import { Button } from 'react-bootstrap';
-import Rating from './Rating';
+// import Rating from './Rating';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return(
       <div>
-       <Rating rating="1"/>
-       <Rating rating="2"/>
-       <Rating rating="3"/>
-       <Rating rating="4"/>
-       <Rating rating="5"/>
+        <Products />
       </div>
     );
   }
