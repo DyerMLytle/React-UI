ec7872275fa929c66311b7f5f233a0bc4d815440
dyerlytle <dlytle@lowell.edu>
2021-11-04 08:23:03 -0700

    Updated 3d stuff a bit, skydome, camera controls.

--- LDT-model/src/index.js
+++ LDT-model/src/index.js
@@ -5,7 +5,9 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <Suspense fallback={<div>Loading... </div>}> */}
+      <App />
+    {/* </Suspense>, */}
   </React.StrictMode>,
   document.getElementById('root')
 );
